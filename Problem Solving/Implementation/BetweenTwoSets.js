2d15938a2f9e636d7b47418b1e52e4498b1df7cd
Bethlehem Woldekidan <nate85here@gmail.com>
2025-07-05 21:04:44 +0300

    Update BetweenTwoSets.js

--- Problem Solving/Implementation/BetweenTwoSets.js
+++ Problem Solving/Implementation/BetweenTwoSets.js
@@ -1,1 +1,88 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'getTotalX' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+function getTotalX(a, b) {
+     // Helper function to find the GCD of two numbers
+    function gcd(x, y) {
+        while (y !== 0) {
+            let temp = y;
+            y = x % y;
+            x = temp;
+        }
+        return x;
+    }
+
+    // Helper function to find the LCM of two numbers
+    function lcm(x, y) {
+        return (x * y) / gcd(x, y);
+    }
+
+    // Step 1: Find the LCM of all elements in array a
+    let lcmA = a.reduce((acc, val) => lcm(acc, val));
+
+    // Step 2: Find the GCD of all elements in array b
+    let gcdB = b.reduce((acc, val) => gcd(acc, val));
+
+    // Step 3: Count the numbers between the two sets
+    let count = 0;
+    for (let i = lcmA; i <= gcdB; i += lcmA) {
+        if (gcdB % i === 0) {
+            count++;
+        }
+    }
+
+    return count;
+    // Write your code here
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const m = parseInt(firstMultipleInput[1], 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const brr = readLine().replace(/\s+$/g, '').split(' ').map(brrTemp => parseInt(brrTemp, 10));
+
+    const total = getTotalX(arr, brr);
+
+    ws.write(total + '\n');
+
+    ws.end();
+}
 
